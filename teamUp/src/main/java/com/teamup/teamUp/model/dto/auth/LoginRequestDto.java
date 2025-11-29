5b2ce03647bc5ffc92b0c51111d7bba6265c5d4c
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2025-11-29 22:07:34 +0200

    validation messages

--- teamUp/src/main/java/com/teamup/teamUp/model/dto/auth/LoginRequestDto.java
+++ teamUp/src/main/java/com/teamup/teamUp/model/dto/auth/LoginRequestDto.java
@@ -3,9 +3,9 @@ package com.teamup.teamUp.model.dto.auth;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequestDto(
-        @NotBlank (message = "This field is required")
+        @NotBlank (message = "Email or username is required.")
         String emailOrUsername,
 
-        @NotBlank (message = "This field is required")
+        @NotBlank (message = "Password is required.")
         String password) {
 }
--- teamUp/src/main/java/com/teamup/teamUp/model/dto/match/MatchCreateRequestDto.java
+++ teamUp/src/main/java/com/teamup/teamUp/model/dto/match/MatchCreateRequestDto.java
@@ -8,30 +8,35 @@ import java.time.Instant;
 import java.util.UUID;
 
 public record MatchCreateRequestDto(
-        @NotNull
+        @NotNull(message = "Select a venue")
         UUID venueId,
 
-        @NotNull @FutureOrPresent
+        @NotNull(message = "Start time is required.")
+        @FutureOrPresent(message = "Start time must be in the present or future.")
         Instant startsAt,
 
-        @NotNull @Positive
+        @NotNull(message = "Duration is required.")
+        @Positive(message = "Duration must be a positive number.")
         Integer durationMinutes,
 
-        @NotNull @Positive
+        @NotNull(message = "Max players is required.")
+        @Positive(message = "Max players must be a positive number.")
         Integer maxPlayers,
 
-        @NotBlank @Size(max = 120)
+        @NotBlank(message = "Title is required.")
+        @Size(max = 120,  message = "Title cannot exceed 120 characters.")
         String title,
 
-        @Size(max = 2000)
+        @Size(max = 2000, message = "Notes cannot exceed 2000 characters.")
         String notes,
 
         MatchVisibility visibility,
 
-        @Future
+        @Future(message = "Join deadline must be in the future.")
         Instant joinDeadline,
 
-        @Digits(integer = 8, fraction = 2)
+        @Digits(integer = 8, fraction = 2,
+                message = "Total price must be a valid amount (up to 2 decimals).")
         BigDecimal totalPrice
 
 ) {
