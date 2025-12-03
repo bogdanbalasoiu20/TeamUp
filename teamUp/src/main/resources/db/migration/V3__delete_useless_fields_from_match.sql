d375a52d0a50500854d2e01696838e9306e3f236
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2025-12-04 00:31:44 +0200

    redeploy

--- teamUp/src/main/resources/db/migration/V3__delete_useless_fields_from_match.sql
+++ teamUp/src/main/resources/db/migration/V3__delete_useless_fields_from_match.sql
@@ -1,2 +1,2 @@
-alter table match drop column if exists current_players;
-alter table match drop column if exists ends_at;
+alter table matches drop column if exists current_players;
+alter table matches drop column if exists ends_at;
