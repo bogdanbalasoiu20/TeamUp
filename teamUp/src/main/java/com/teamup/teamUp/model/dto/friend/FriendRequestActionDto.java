cd5c676d536c4617a2d45bfcc3100f4e803fce2a
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2025-12-10 18:23:52 +0200

    @JsonProperty added in FriendRequestActionDto

--- teamUp/src/main/java/com/teamup/teamUp/model/dto/friend/FriendRequestActionDto.java
+++ teamUp/src/main/java/com/teamup/teamUp/model/dto/friend/FriendRequestActionDto.java
@@ -1,9 +1,11 @@
 package com.teamup.teamUp.model.dto.friend;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 public record FriendRequestActionDto(
         @NotNull
+        @JsonProperty("accept")
         Boolean accept
 ) {
 }
