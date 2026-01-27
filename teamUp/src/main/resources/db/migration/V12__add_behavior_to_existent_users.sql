ec0f8c0c1e6ad7c0677566af9741f3a0c8de6cff
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2026-01-27 16:48:03 +0200

    migration V2

--- teamUp/src/main/resources/db/migration/V12__add_behavior_to_existent_users.sql
+++ teamUp/src/main/resources/db/migration/V12__add_behavior_to_existent_users.sql
@@ -1,12 +1,18 @@
+ALTER TABLE player_behavior_stats
+    ALTER COLUMN id SET DEFAULT gen_random_uuid();
+
+
 INSERT INTO player_behavior_stats (
     user_id,
     fair_play,
+    competitiveness,
     communication,
     fun,
-    competitiveness,
     adaptability,
     reliability,
-    feedback_count
+    feedback_count,
+    created_at,
+    updated_at
 )
 SELECT
     u.id,
@@ -16,8 +22,11 @@ SELECT
     70,
     70,
     70,
-    0
+    0,
+    NOW(),
+    NOW()
 FROM users u
          LEFT JOIN player_behavior_stats pbs
                    ON pbs.user_id = u.id
 WHERE pbs.user_id IS NULL;
+
