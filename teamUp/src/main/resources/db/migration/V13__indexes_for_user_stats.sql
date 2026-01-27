d75a9891d90143047165b46c157569f871dcfbcb
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2026-01-28 01:07:45 +0200

    migration error

--- teamUp/src/main/resources/db/migration/V13__indexes_for_user_stats.sql
+++ teamUp/src/main/resources/db/migration/V13__indexes_for_user_stats.sql
@@ -1,5 +1,5 @@
 CREATE INDEX idx_pr_rater_user ON player_ratings(rater_user_id);
 CREATE INDEX idx_pr_rated_user ON player_ratings(rated_user_id);
-CREATE INDEX idx_match_created_by ON matches(created_by);
-CREATE INDEX idx_match_player_user ON match_players(user_id);
+CREATE INDEX idx_match_created_by ON matches(creator);
+CREATE INDEX idx_match_player_user ON match_participants(user_id);
 CREATE INDEX idx_stats_history_user ON player_card_stats_history(user_id);
