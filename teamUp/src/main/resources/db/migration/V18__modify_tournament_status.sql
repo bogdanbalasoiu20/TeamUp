26a1c7b7e38fcf9b639998a5534d8e02eaa88e5e
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2026-02-21 21:15:47 +0200

    deploy failed

--- teamUp/src/main/resources/db/migration/V18__modify_tournament_status.sql
+++ teamUp/src/main/resources/db/migration/V18__modify_tournament_status.sql
@@ -1,13 +1,14 @@
-UPDATE tournaments
-SET status = 'ONGOING'
-WHERE status = 'STARTED';
-
 ALTER TYPE tournament_status RENAME TO tournament_status_old;
 
 CREATE TYPE tournament_status AS ENUM ('OPEN', 'ONGOING', 'FINISHED');
 
 ALTER TABLE tournaments
 ALTER COLUMN status TYPE tournament_status
-    USING status::text::tournament_status;
+USING (
+    CASE status::text
+        WHEN 'STARTED' THEN 'ONGOING'
+        ELSE status::text
+    END
+)::tournament_status;
 
 DROP TYPE tournament_status_old;
