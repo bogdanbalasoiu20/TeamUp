db62dfa418a3421ac1958062dcbf6b99707815be
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2026-03-20 20:01:25 +0200

    bigger difference than expected between odds

--- teamUp/src/main/java/com/teamup/teamUp/service/MatchOddsService.java
+++ teamUp/src/main/java/com/teamup/teamUp/service/MatchOddsService.java
@@ -31,7 +31,7 @@ public class MatchOddsService {
 
         double max = Math.max(homeScore, awayScore);
 
-        double scaleFactor = 0.08;
+        double scaleFactor = 0.05;
 
         double expHome = Math.exp((homeScore - max) * scaleFactor);
         double expAway = Math.exp((awayScore - max) * scaleFactor);
@@ -41,6 +41,10 @@ public class MatchOddsService {
         double pAway = expAway / baseTotal;
 
         double diff = Math.abs(homeScore - awayScore);
+
+        if (diff < 5) {
+            diff *= 0.5;
+        }
 
         double pDraw = 0.30 * Math.exp(-0.05 * diff);
         pDraw = clamp(pDraw, 0.18, 0.30);
